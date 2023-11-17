59153e21bf731cab03fbe9bc752f5e1ca61e7a48
adarshkh2397 <adarshkhandelwal.work@gmail.com>
2023-11-17 02:36:17 -0500

    Add new Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,24 +1,43 @@
+import datetime
 from django.db import models
 from django.utils.timezone import now
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=50, default="name")
+    description = models.CharField(max_length=200, default="description")
+    
+    #course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = "Mini"
+    TRUCK = "Truck"
+    CAR_MODES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, "Mini"),
+        (TRUCK,"Truck")
+    ]
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, default="name")
+    dealer_id = models.IntegerField(default=50)
+    model_type = models.CharField(max_length=15, choices=CAR_MODES, default=SEDAN)
 
+    YEAR_CHOICES = []
+    for r in range(1985, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r, r))
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    year = models.IntegerField(
+        ('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+    def __str__(self):
+        return self.name + ", " + str(self.year) + ", " + self.model_type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
